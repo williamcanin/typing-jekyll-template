05b39912a3d9a50e973843aaa716705c8779a80b
William C. Canin <william.costa.canin@gmail.com>
2017-11-11 19:24:34 -0200

    New version 1.0.3

--- assets/javascripts/resume.js
+++ assets/javascripts/resume.js
@@ -3,8 +3,22 @@ layout: null
 ---
 
 jQuery(document).ready(function($) {
+  /* Method 2: */
+  /* var isFirefox = /^((?!chrome|android).)*firefox/i.test(navigator.userAgent); */
   $("#btn-print").click(function() {
-    window.print();
-    return false;
+    /* Method 1:*/
+    if (navigator.vendor ==  "" || navigator.vendor == undefined) {
+      alert("This Browser is not printable with this page. If you print with Ctrl + P, errors will appear in the page structure. We recommend 'Google Chrome' or 'Safari'. \n\n(Este Navegador não é compátivel com impressão desta página. Caso imprima com Ctrl+P, aparecerá erros na estrutura da página. Recomendamos o 'Google Chrome' ou 'Safari'.)");
+    } else{
+      window.print();
+      return false;
+    }
+    /* Method 2: */
+    /*if (isFirefox == true) {
+      alert("This Browser is not printable with this page. If you print with Ctrl + P, errors will appear in the page structure. We recommend 'Google Chrome' or 'Safari'. \n\n(Este Navegador não é compátivel com impressão desta página. Caso imprima com Ctrl+P, aparecerá erros na estrutura da página. Recomendamos o 'Google Chrome' ou 'Safari'.)");
+    } else {
+      window.print();
+      return false;
+    }*/
   });
 });
