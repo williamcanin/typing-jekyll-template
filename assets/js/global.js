d37647fae50f4250e00a7f73fdeb914f6d58735a
William C. Canin <william.costa.canin@gmail.com>
2019-10-03 21:49:26 -0300

    qui out  3 21:49:26 -03 2019

--- assets/js/global.js
+++ assets/js/global.js
@@ -2,9 +2,13 @@
 layout: null
 ---
 
-$(document).ready(function() {
+jQuery(document).ready(function() {
 	$('.scrolltop__button').click(function(){
 		$('html, body').animate({scrollTop:0}, 'slow');
 		return false;
 	});
+	$("#comments_in_post").click(function(event){     
+        event.preventDefault();
+        $('html,body').animate({scrollTop:$(this.hash).offset().top}, 500);
+    });
 });
