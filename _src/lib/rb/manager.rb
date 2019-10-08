51f24137dfa977f6e5f8d79ba1340186873c77fe
William C. Canin <william.costa.canin@gmail.com>
2019-10-08 20:16:49 -0300

    Update - 2019-10-08T20:16:49-03:00

--- _src/lib/rb/manager.rb
+++ _src/lib/rb/manager.rb
@@ -210,7 +210,13 @@ class Manager
         cd #{dir}; git push #{push_origin} #{push_branch}
         """
         # puts push_start
-        system(push_start)
+        Open3.popen3(push_start) do |stdin, stdout, stderr, wait_thr|
+          stdin.puts "Start push...".blue
+          stdin.close                # we're done
+          stdout_str = stdout.read   # read stdout to string. note that this will block until the command is done!
+          stderr_str = stderr.read   # read stderr to string
+          status = wait_thr.value    # will block until the command finishes; returns status that responds to .success? etc
+        end
 
         puts "Deploy, Done!".green
       
